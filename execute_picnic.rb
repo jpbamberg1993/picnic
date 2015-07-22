4af60364f6e00792a2b7eaab009b46dcff3d95b4
John Paul Bamberg <jpbamberg1993@gmail.com>
2015-07-22 19:13:38 -0400

    actually working / object oriented

--- execute_picnic.rb
+++ execute_picnic.rb
@@ -1,12 +1,13 @@
-require "./refactor_test_1.rb"
+require_relative "./refactor_test_1"
 
 @food_needed = ["corn", "hamburger", "swiss cheese", "american cheese", "hamburger buns", "hotdog buns", "watermelon", "pasta", "potato salad", "vegetable", "condiments", "ketchup", "mustard", "relish", "salt", "pepper" ];
 @drinks_needed = ["beer", "ice tea", "water", "soda"];
-@supplies_needed = ["ice", "bags of ice", "ice pack", "cups", "plates", "cup", "plate", "utensils", "fork", "knife", "spoon", "big spoon", "napkins", "trashbag", "trash bag", "moist towels", "paper towels", "a really cute picnic basket", "volleyball" ];
+@supplies_needed = ["ice", "bags of ice", "ice pack", "cups", "plates", "cup", "plate", "utensils", "fork", "knife", "spoon", "big spoon", "napkins", "trashbag", "trash bag", "moist towels", "paper towels", "a really cute picnic basket", "volleyball"];
 @supplied_by_people = [];
 
-welcome_first_person = Greet.new(@food_needed, @drinks_needed, @supplies_needed)
-welcome_first_person.display_greeting
-
-choose_brought = ExecutePicnic.new(@food_needed, @drinks_needed)
+choose_brought = ExecutePicnic.new(@food_needed, @drinks_needed, @supplies_needed, @supplied_by_people)
+choose_brought.display_greeting
 choose_brought.bringing_food
+choose_brought.bringing_supplies
+choose_brought.bringing_drinks
+choose_brought.get_name
